1c2fd9fc480712e18805a3ea98368d4af7a0e0fc
JohnM101 <johnmichaelolediana@gmail.com>
2025-09-08 01:13:41 +0800

    Your commit message here

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://dbUs3r:SN8DdNQvyCnXXJf2@cluster0.q1owwie.mongodb.net/bookStore';
+    const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://dbUs3r:SN8DdNQvyCnXXJf2@cluster.sfo5yxw.mongodb.net/bookStore';
     const conn = await mongoose.connect(mongoURI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
