acac71788fe13896dc6d86a53ddbfe54f1b83426
JohnM101 <johnmichaelolediana@gmail.com>
2025-09-08 01:37:12 +0800

    Message

--- server/server.js
+++ server/server.js
@@ -82,9 +82,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../build')));
 
   // Handle React routing, return all requests to React app
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
-  });
+  app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 }
 
 // Start server
