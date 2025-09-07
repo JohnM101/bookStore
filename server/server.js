80d4f82a7dc07d5f5a74481d5b2426f9dd535aa5
JohnM101 <johnmichaelolediana@gmail.com>
2025-09-08 01:41:30 +0800

    Message

--- server/server.js
+++ server/server.js
@@ -80,9 +80,7 @@ createAdminUser();
 if (process.env.NODE_ENV === 'production') {
   // Serve static files from the React app
   app.use(express.static(path.join(__dirname, '../build')));
-
-  // Handle React routing, return all requests to React app
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 }
