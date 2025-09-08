b398e03d88f1692f899d85777230fa4df61fdb1d
JohnM101 <johnmichaelolediana@gmail.com>
2025-09-08 19:39:24 +0800

    Message

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 
 ;// Enable CORS
 app.use(cors({
-  origin: ['https://anime-you-one.vercel.app', 'http://localhost:3000']
+  origin: ['https://book-store-azure-chi.vercel.app', 'http://localhost:3000']
 }))
 
 // Middleware
