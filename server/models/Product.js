46eaedb9f519a056c4e2e90cb1c0a93f1caceee8
JohnM101 <johnmichaelolediana@gmail.com>
2025-09-21 11:48:44 +0800

    Upload/Update project with latest progress

--- server/models/Product.js
+++ server/models/Product.js
@@ -9,6 +9,7 @@ const productSchema = new mongoose.Schema({
   description: { type: String, required: true },
   category: { type: String, required: true },
   subcategory: { type: String, required: true },
+  CountInStock: { type: Number, required: true },
   inStock: { type: Boolean, default: true },
   rating: { type: Number },
   sizes: [String] // For clothing items
