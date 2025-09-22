cbd73ac0ce31c49b8869cad61746bca5a4451b1f
JohnM101 <johnmichaelolediana@gmail.com>
2025-09-23 00:42:03 +0800

    Upload/Update project with latest progress

--- server/server.js
+++ server/server.js
@@ -43,6 +43,8 @@ app.use('/uploads', express.static('uploads'));
 // Routes
 app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/cart', require('./routes/cartRoutes'));
+
+app.use('/api/cart', require('./routes/addToCartRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/admin', adminRoutes);
 app.use('/api/auth', authRoutes);
